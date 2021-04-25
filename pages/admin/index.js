c3f48cfc8284558f1f7e347c92e3ab0faaacf0ae
Nodarra <sisicdino4@gmail.com>
2021-04-25 20:11:38 +0200

    Added app routes and Navbar

---
+++ pages/admin/index.js
@@ -0,0 +1,7 @@
+export default function AdminPostsPage() {
+  return (
+    <main>
+      <h1>Admin post</h1>
+    </main>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import Loader from '../components/Loader'
+
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div>
-      Hello World
+      <button onClick={() => toast.success('Hello Toast!!!')}>
+        Toast Me
+      </button>
     </div>
   )
 }
